e2f3c23fe865ec3dd3b436ead823aa8ca274a3f8
lpastenpinto <lpastenpinto@gmail.com>
2016-04-02 18:04:21 -0400

    Icono/Login

---
+++ SoftwareCelta/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareCelta.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(FormCollection post)
+        {
+            Session["userID"] = "User";
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Contact", "Home");
+        }
+    }
+}
